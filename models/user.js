6d07fcce2b1718b297420c7b60693f92efb54034
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2024-01-25 21:05:56 +0100

    Added forgot password and reset password

--- models/user.js
+++ models/user.js
@@ -5,9 +5,11 @@ const passportLocalMongoose = require('passport-local-mongoose')
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minLength: 8 },
     isVerified: { type: Boolean, required: true, default: false },
+    passowrdResetOtp: { type: String, },
+    passworOtpExpires: { type: Date, },
 });
 
 userSchema.plugin(passportLocalMongoose)
