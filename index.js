f71f24bc6dfac1690e6e1481e21a5934a20101c7
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2024-01-26 12:45:37 +0100

    Added jwt to login

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const passport = require('passport');
-const connectDB = require ('./config/db')
+const connectDB = require('./config/db');
 const app = express();
 require('dotenv').config();
-const router = require ('./router/handler')
+const router = require('./router/handler');
 const port = process.env.PORT || 3000;
 
-//Middlewares
-
-app.use(express.json())
-
-app.use(passport.initialize())
-
-app.use ('/api/v1', router)
 
 
-app.listen (port, () =>{
+app.use(express.json());
+
+app.use(passport.initialize());
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+app.use('/api/v1', router);
+
+
+app.listen(port, () => {
     connectDB()
     console.log(`Server started on port ${port}`);
-})
+});
